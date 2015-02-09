0f2837586efe16432eab9c49361fc4486cfe82a5
nirjashah3 <nirjashah3@users.noreply.github.com>
2015-02-09 13:13:26 +0530

    button

---
+++ button2.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script type="text/javascript" src="jquery.simple-dtpicker.js"></script>
+	<link type="text/css" href="jquery.simple-dtpicker.css" rel="stylesheet" />
+	
+	<style type="text/css">
+		body { background-color: #fefefe; padding-left: 2%; padding-bottom: 100px; color: #101010; }
+		footer{ font-size:small;position:fixed;right:5px;bottom:5px; }
+		a:link, a:visited  { color: #0000ee; }
+		pre{ background-color: #eeeeee; margin-left: 1%; margin-right: 2%; padding: 2% 2% 2% 5%; }
+		p { font-size: 0.9rem; }
+		ul { font-size: 0.9rem; }
+		hr { border: 2px solid #eeeeee; margin: 2% 0% 2% -3%; }
+		h3 { border-bottom: 2px solid #eeeeee; margin: 2rem 0 2rem -1%; padding-left: 1%; padding-bottom: 0.1em; }
+		h4 { border-bottom: 1px solid #eeeeee; margin-top: 2rem; margin-left: -1%; padding-left: 1%; padding-bottom: 0.1em; }
+		
+	</style>
+	
+	
+</head>
+<body>
+			<img src="syn-logo-withTagLine.png" width="283" height="56"/>
+
+		</body>
+	<center><h2><b>Library Options</b></h2></center>
+
+
+<center>
+<button onclick="window.location='http://localhost:7777/t.php'">Check Availability</button><br><br><br>
+<button onclick="window.location='http://localhost:7777/wish.php'">Add to Wishlist</button>
+</center>
+
+</html>
